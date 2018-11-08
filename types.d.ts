7589c87f7cee5fa23e5332db51ad46b27f404645
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-11-08 11:25:39 +0000

    Depend on @types/setasap.
    Remove local type definitions for setasap.

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,3 @@
-declare module "setasap" {
-    export = setAsap;
-    function setAsap(handler: (() => void) | string): void;
-}
-
 declare module "promise-polyfill" {
     export interface PromisePolyfillConstructor extends PromiseConstructor {
         _immediateFn?: (handler: (() => void) | string) => void;
