0a2e2ddee1d875869f2c7bbb180b7d840768edd6
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-11-21 07:54:19 +0000

    Replace custom type definitions with @types/promise-polyfill.

--- types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "promise-polyfill" {
-    export interface PromisePolyfillConstructor extends PromiseConstructor {
-        _immediateFn?: (handler: (() => void) | string) => void;
-    }
-
-    const Promise: PromisePolyfillConstructor;
-
-    export default Promise;
-}
